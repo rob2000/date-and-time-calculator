aec70fbf178e2faf5114e1d4a91dd35a49eb5983
robertsc <robertsc@ROBERTSCHALLER.intranet.21torr.com>
2010-12-22 18:05:22 +0100

    initial commit

---
+++ src/com/robertschaller/android/dtcalc/DateAndTimeCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.robertschaller.android.dtcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DateAndTimeCalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
