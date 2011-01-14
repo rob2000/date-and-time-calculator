6add98c673771d0b0f17218dbf2f072bab71f3c9
robertsc <robertsc@ROBERTSCHALLER.intranet.21torr.com>
2011-01-14 13:46:50 +0100

    added operations to stack

--- src/com/robertschaller/android/dtcalc/components/Stack.java
+++ src/com/robertschaller/android/dtcalc/components/Stack.java
@@ -1,31 +1,60 @@
 package com.robertschaller.android.dtcalc.components;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.joda.time.DateTime;
 import org.joda.time.Period;
 
+import com.robertschaller.android.dtcalc.operators.AbstractOperator;
+import com.robertschaller.android.dtcalc.operators.IllegalOperationException;
+
 public class Stack {
 
+  /** The date time result. */
   protected DateTime dateTimeResult;
   
-  protected Period datePeriodResult;
+  /** The period result. */
+  protected Period periodResult;
   
+  /** The calculation. */
   protected String calculation;
   
+  /** The date mode. */
   protected boolean dateMode;
   
+  /**
+   * Instantiates a new stack.
+   */
   public Stack(){
     dateMode = false;
   }
   
+  /**
+   * Push.
+   *
+   * @param operation the operation
+   * @param period the period
+   */
   public void push( String operation, Period period ){
-      
+    if( dateMode ){
+      dateTimeResult = AbstractOperator.instanceOf( operation ).operate( dateTimeResult, period );
+    }else{
+      periodResult = AbstractOperator.instanceOf( operation ).operate( periodResult, period );
+    }
   }
   
-  public void push( String operation, DateTime dateTime ){
-    dateMode = true;
+  /**
+   * Push.
+   *
+   * @param operation the operation
+   * @param dateTime the date time
+   * @throws IllegalOperationException the illegal operation exception
+   */
+  public void push( String operation, DateTime dateTime ) throws IllegalOperationException{
+    if( dateMode ){
+      throw( new IllegalOperationException( "date can't be added to date" ) );
+    }else{
+      dateTimeResult = AbstractOperator.instanceOf( operation ).operate( dateTime, periodResult );
+      dateMode = true;
+    }
   }
   
 }
---
+++ src/com/robertschaller/android/dtcalc/operators/IllegalOperationException.java
@@ -0,0 +1,9 @@
+package com.robertschaller.android.dtcalc.operators;
+
+public class IllegalOperationException extends Exception {
+
+  public IllegalOperationException(String detailMessage) {
+    super( detailMessage );
+  }
+
+}
