1630ff2062cd51a8b488e437fb33f6a1fa7ccac4
robertsc <robertsc@ROBERTSCHALLER.intranet.21torr.com>
2011-01-14 11:41:45 +0100

    got rid of value

--- src/com/robertschaller/android/dtcalc/operators/AbstractOperator.java
+++ src/com/robertschaller/android/dtcalc/operators/AbstractOperator.java
@@ -1,7 +1,12 @@
 package com.robertschaller.android.dtcalc.operators;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+
 public abstract class AbstractOperator {
 
-  abstract public Value operate( Value firstValue, Value secondValue );
+  abstract public DateTime operate( DateTime firstValue, Period secondValue );
+  
+  abstract public Period operate( Period firstValue, Period secondValue );
 
 }
--- src/com/robertschaller/android/dtcalc/operators/PlusOperator.java
+++ src/com/robertschaller/android/dtcalc/operators/PlusOperator.java
@@ -1,10 +1,19 @@
 package com.robertschaller.android.dtcalc.operators;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+
 
 public class PlusOperator extends AbstractOperator {
 
   @Override
-  public Value operate( Value firstValue, Value secondValue ) {
+  public DateTime operate(DateTime firstValue, Period secondValue) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Period operate(Period firstValue, Period secondValue) {
     // TODO Auto-generated method stub
     return null;
   }
